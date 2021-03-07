d01dc5c14b2fc76c5d36cd2c5376801dea02af5d
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-03-07 11:32:55 +0900

    レンダリングの最適化 memo使用

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,11 +1,19 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "aqua"
 }
 
-export const ChildArea = (props) => {
+export const ChildArea = memo((props) => {
   const { open } = props;
+  console.log("childAreaがレンダリングされた");
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
   return (
     <>
       {open ? (
@@ -15,4 +23,4 @@ export const ChildArea = (props) => {
       ): null}
     </>
   )
-}
+});
