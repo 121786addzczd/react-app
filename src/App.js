5c4c6025bc73a17f6832b611e5a32084ec6a0b74
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-03-07 11:56:06 +0900

    useCallbackで関数をmemo化

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -12,13 +12,15 @@ export default function App() {
 
   const onClickOpen = () => setOpen(!open);
 
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText}/>
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
